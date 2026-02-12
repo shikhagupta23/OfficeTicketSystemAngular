0fd145258311ed3cec76e56c9214279418174096
shikhagupta23 <shikhagupta@resiliencesoft.com>
2026-02-12 11:16:19 +0530

    Initial Angular project commit

--- src/app/constants/api-endpoints.ts
+++ src/app/constants/api-endpoints.ts
@@ -3,13 +3,13 @@ export const API_ENDPOINTS = {
     LOGIN: 'https://officeticketsystemdotnet-production.up.railway.app/api/AuthApi/login'
   },
   TICKET: {
-    RAISE: 'https://localhost:7166/api/TicketAPI/raise',
-    MY_TICKETS: 'https://localhost:7166/api/TicketAPI',
-    GET_ADMINS: 'https://localhost:7166/api/TicketAPI/admins',
-    ASSIGN_TICKET: 'https://localhost:7166/api/TicketAPI/assign-ticket',
-    RESOLVE: 'https://localhost:7166/api/TicketAPI/resolve'  },
+    RAISE: 'https://officeticketsystemdotnet-production.up.railway.app/api/TicketAPI/raise',
+    MY_TICKETS: 'https://officeticketsystemdotnet-production.up.railway.app/api/TicketAPI',
+    GET_ADMINS: 'https://officeticketsystemdotnet-production.up.railway.app/api/TicketAPI/admins',
+    ASSIGN_TICKET: 'https://officeticketsystemdotnet-production.up.railway.app/api/TicketAPI/assign-ticket',
+    RESOLVE: 'https://officeticketsystemdotnet-production.up.railway.app/api/TicketAPI/resolve'  },
 
   DASHBOARD: {
-    STATS: 'https://localhost:7166/api/dashboard/stats'
+    STATS: 'https://officeticketsystemdotnet-production.up.railway.app/api/dashboard/stats'
   }
 };
